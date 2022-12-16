8f0de1e2f805ef5feef632c4b1dd0411c6ad7887
mandril888 <arturo.rubio.89.8@gmail.com>
2022-12-16 10:14:44 +0100

    recursive X tree

--- src/ChristmasTree.vue
+++ src/ChristmasTree.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <div v-for="index in size" :key="index" class="flex flex-row justify-center">
+    <ChristmasTree v-if="size > 1" :size="size - 1" />
+    <div class="flex flex-row justify-center">
+      <div
+        v-for="index in size"
+        :key="index"
+        class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center"
+      />
+    </div>
+    <!-- <div v-for="index in size" :key="index" class="flex flex-row justify-center">
       <div
         v-for="i in index"
         :key="i"
         class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center"
       />
-    </div>
+    </div> -->
   </div>
 </template>
 
