eadff376a1ac23c7de192dbf07b68570e78e4d07
mandril888 <arturo.rubio.89.8@gmail.com>
2022-12-16 10:04:05 +0100

    X tree done

--- src/ChristmasTree.vue
+++ src/ChristmasTree.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <div class="flex flex-row justify-center">
-      <!-- This div is styled for the tree sections, but it's not complete -->
-      <div class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+    <div v-for="index in size" :key="index" class="flex flex-row justify-center">
+      <div
+        v-for="i in index"
+        :key="i"
+        class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center"
+      />
     </div>
   </div>
 </template>
